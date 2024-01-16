3c7db77ef8b5ffd5dec77866aff80d437e60ae18
yotuki-tuyu <119393226+yotuki-tuyu@users.noreply.github.com>
2024-01-16 17:28:40 +0900

    Embeds Edit

--- commands/utility/report.js
+++ commands/utility/report.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, userMantion, EmbedBuilder, Colors } = require('discord.js');
+const { SlashCommandBuilder, userMention, EmbedBuilder, Colors } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -20,8 +20,12 @@ module.exports = {
 	async execute(interaction) {
         const addreportchannel = interaction.options.getString("addreportchannel");    //IDには、「.setName」で指定した名前を指定
 		await interaction.reply(`インシデント報告用チャンネルを作成しました。チャンネル名は: ${addreportchannel}です。カテゴリー:インシデントログで確認してください`);
+        const channel = await interaction.guild.channels.create({
+            name: addreportchannel,
+            parent: '1195712382798930022',
+        });
         await interaction. client.channels.cache.get('1195747894704209960').send({
-            content: userMantion('1074320635855126538'),
+            content: userMention('1074320635855126538'),
             embeds: [
                 new EmbedBuilder()
                 .setName('インシデントが発生しました。')
@@ -29,12 +33,9 @@ module.exports = {
                 .addFields(
                     { name: 'インシデント報告名:', value: addreportchannel}
                 )
+                .setFooter({ text: channelMention(channel.id) })
                 .setColor(Colors.Blue)
             ]
-        });
-        await interaction.guild.channels.create({
-            name: addreportchannel,
-            parent: '1195712382798930022',
         });
 	},
 };
