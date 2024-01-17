43bfb667dae5eef1934396ab43e97d8e52d13039
yotuki-tuyu <119393226+yotuki-tuyu@users.noreply.github.com>
2024-01-17 02:45:24 +0000

    trainreport create closed command

--- commands/utility/report.js
+++ commands/utility/report.js
@@ -16,34 +16,60 @@ module.exports = {
             .setDescription('インシデント報告名')
             .setRequired(true),
             )
+            )
+            .addSubcommand((subcommand) =>
+            subcommand
+            .setName("closed")
+            .setDescription("インシデント報告チャンネルを閉じる")
             ),
 	async execute(interaction) {
-        const addreportchannel = interaction.options.getString("addreportchannel");    //IDには、「.setName」で指定した名前を指定
-        const channel = await interaction.guild.channels.create({
-            name: addreportchannel,
-            parent: '1195712382798930022',
-        });
-		await interaction.reply({ embeds: [
-            new EmbedBuilder()
-            .setTitle('インシデント報告用チャンネルを作成しました。')
-            .setDescription('カテゴリー:インシデントログで確認してください')
-            .addFields(
-                { name: 'チャンネル名:', value: channelMention(channel.id)}
-            )
-            .setColor(Colors.Blue)
-        ]
-    });
-        await interaction.client.channels.cache.get('1195747894704209960').send({
-            content: userMention('1074320635855126538'),
-            embeds: [
+        if (interaction.options.getSubcommand() === 'add') {
+			
+            const addreportchannel = interaction.options.getString("addreportchannel");    //IDには、「.setName」で指定した名前を指定
+            const channel = await interaction.guild.channels.create({
+                name: addreportchannel,
+                parent: '1195712382798930022',
+            });
+            await interaction.reply({ embeds: [
                 new EmbedBuilder()
-                .setTitle('インシデントが発生しました。')
-                .setDescription('対応してください。')
+                .setTitle('インシデント報告用チャンネルを作成しました。')
+                .setDescription('カテゴリー:インシデントログで確認してください')
                 .addFields(
-                    { name: 'インシデント報告名:', value: channelMention(channel.id)}
+                    { name: 'チャンネル名:', value: channelMention(channel.id)}
                 )
                 .setColor(Colors.Blue)
             ]
         });
+            await interaction.client.channels.cache.get('1195747894704209960').send({
+                content: userMention('1074320635855126538'),
+                embeds: [
+                    new EmbedBuilder()
+                    .setTitle('インシデントが発生しました。')
+                    .setDescription('対応してください。')
+                    .addFields(
+                        { name: 'インシデント報告名:', value: channelMention(channel.id)}
+                    )
+                    .setColor(Colors.Blue)
+                ]
+            });
+		} else if (interaction.options.getSubcommand() === 'closed') {
+            if (interaction.channel.parentId !== '1195712382798930022') {
+                await interaction.reply(`インシデントログでのみ実行可能です。`);
+            } else {
+                await interaction.reply(`このチャンネルを閉じます・・・`);
+                await interaction.client.channels.cache.get('1195747894704209960').send({
+                    content: userMention('1074320635855126538'),
+                    embeds: [
+                        new EmbedBuilder()
+                        .setTitle('解決済み。')
+                        .addFields(
+                            { name: 'インシデント報告名:', value: interaction.channel.name}
+                        )
+                        .setColor(Colors.Blue)
+                    ]
+                });
+                await interaction.channel.delete();
+            }
+		}
 	},
 };
