ca5648a31781e43bfd92bc1c2f2f2d7560625263
2 1 <InotitoKokoro@gmail.com>
2024-02-26 22:34:21 +0900

    elseとルールは忘れるな_#1
    else付け忘れを直した。

--- commands/utility/announce.js
+++ commands/utility/announce.js
@@ -76,160 +76,160 @@ module.exports = {
                     .setRequired(false),
                 )
         ),
-        /**
-     * インタラクションが作成されたときに呼ばれるイベントのリスナー関数
-     * @param {ChatInputCommandInteraction} interaction
-     */
+    /**
+ * インタラクションが作成されたときに呼ばれるイベントのリスナー関数
+ * @param {ChatInputCommandInteraction} interaction
+ */
 
-        //ロールなしエラー
+    //ロールなしエラー
     async execute(interaction) {
         if (!interaction.member.roles.cache.has('1192986404142207136')) {
             await interaction.reply({ content: `エラー:実行権限がありませんでした。\n実行権限を持ち合わせている場合、めだころかまんめんさんに連絡してください。`, ephemeral: true });
-        }
-        //botid = botid + 1;
+        } else
+            //botid = botid + 1;
 
-        //subcommand == 運転停止/遅延発生情報
-        if (interaction.options.getSubcommand() === 'train_stop') {
+            //subcommand == 運転停止/遅延発生情報
+            if (interaction.options.getSubcommand() === 'train_stop') {
 
-            //変数に代入
-            const stop_section = interaction.options.getString("stop_section");
-            const stop_reason = interaction.options.getString("stop_reason");
-            const stop_repire = interaction.options.getString("stop_repire");
-            const stop_discoverer = interaction.options.getMember("stop_discoverer");
-            const stop_info = interaction.options.getString("stop_info") ?? "なし";
-            var stop_type =  interaction.options.getString("stop_type");
+                //変数に代入
+                const stop_section = interaction.options.getString("stop_section");
+                const stop_reason = interaction.options.getString("stop_reason");
+                const stop_repire = interaction.options.getString("stop_repire");
+                const stop_discoverer = interaction.options.getMember("stop_discoverer");
+                const stop_info = interaction.options.getString("stop_info") ?? "なし";
+                var stop_type = interaction.options.getString("stop_type");
 
-            //現在時刻取得
-            var now = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
-            
-            var Year = now.getFullYear();
-            var Month = now.getMonth()+1;
-            var Day = now.getDate();
-            var Hour = now.getHours();
-            var Min = now.getMinutes();
-            var Sec = now.getSeconds();
+                //現在時刻取得
+                var now = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
 
-            var now_time = Year + "/" + Month + "/" + Day + " " + Hour + ":" + Min + ":" + Sec;
-            
-            
-            //apiにして鯖に送る
-            const https = require("https"); // HTTPのモジュール
-            //ここでJsonのもととなるオブジェクトを作る
-            let json = { 
-                train:{
-                trainstop_information:{
-                    type:stop_type,
-                    stop_section:stop_section,
-                    stop_reason:stop_reason,
-                    stop_repire:stop_repire,
-                    stop_info:stop_info
-                },
-                stop_train_time:now_time
-            },
-            }
-            try {
-                const request = https.request(
-                    //下のURLをいじる
-                    "https://coherent-ruling-quagga.ngrok-free.app/medakoro_1", { method: 'POST', });
-                request.write(JSON.stringify(json))
-                request.end()
-            } catch (ex) {
-                //await interaction.editReply("put失敗！")
-            }
-            //await interaction.editReply("putしました。")
+                var Year = now.getFullYear();
+                var Month = now.getMonth() + 1;
+                var Day = now.getDate();
+                var Hour = now.getHours();
+                var Min = now.getMinutes();
+                var Sec = now.getSeconds();
 
-            //変数を変換
-            if (stop_type == "trainstop") {
-                stop_type = "運転停止";
-            } else if (stop_type == "traindelay") {
-                stop_type = "遅延発生";
-            } else {
+                var now_time = Year + "/" + Month + "/" + Day + " " + Hour + ":" + Min + ":" + Sec;
+
+
+                //apiにして鯖に送る
+                const https = require("https"); // HTTPのモジュール
+                //ここでJsonのもととなるオブジェクトを作る
+                let json = {
+                    train: {
+                        trainstop_information: {
+                            type: stop_type,
+                            stop_section: stop_section,
+                            stop_reason: stop_reason,
+                            stop_repire: stop_repire,
+                            stop_info: stop_info
+                        },
+                        stop_train_time: now_time
+                    },
+                }
+                try {
+                    const request = https.request(
+                        //下のURLをいじる
+                        "https://coherent-ruling-quagga.ngrok-free.app/medakoro_1", { method: 'POST', });
+                    request.write(JSON.stringify(json))
+                    request.end()
+                } catch (ex) {
+                    //await interaction.editReply("put失敗！")
+                }
+                //await interaction.editReply("putしました。")
+
+                //変数を変換
+                if (stop_type == "trainstop") {
+                    stop_type = "運転停止";
+                } else if (stop_type == "traindelay") {
+                    stop_type = "遅延発生";
+                } else {
+                    await interaction.reply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle('Error :', stop_type)
+                                .setColor(Colors.DarkPurple)
+                        ]
+                    });
+                }
+
+                //埋め込み
                 await interaction.reply({
                     embeds: [
                         new EmbedBuilder()
-                        .setTitle('Error :', stop_type)
-                        .setColor(Colors.DarkPurple)
+                            .setTitle('鉄道状態アナウンス[運転停止/遅延発生情報]を発表しました。')
+                            .setColor(Colors.Red)
                     ]
                 });
-            }
 
-            //埋め込み
-            await interaction.reply({
-                embeds: [
-                    new EmbedBuilder()
-                    .setTitle('鉄道状態アナウンス[運転停止/遅延発生情報]を発表しました。')
-                    .setColor(Colors.Red)
-                ]
-            });
-            
-            await interaction.client.channels.cache.get('1195747894704209960').send({
-                content: "📣鉄道状態アナウンスが発令されました",
-                embeds: [
-                    new EmbedBuilder()
-                    .setAuthor({ name: `通達者: ${interaction.member.displayName}` })
-                    .setTitle('鉄道状態アナウンス[運転停止/遅延発生情報 ID:')
-                    .setFields([
-                        { name: '今回のタイプ:', value: stop_type , inline: false},
-                        { name: '💥停止区間:', value: stop_section ,inline: false},
-                        { name: '🧨原因:', value: stop_reason ,inline: false},
-                        { name: '🔨予想修理時間:', value: stop_repire ,inline: false},
-                        { name: '💬詳細情報:', value: stop_info ,inline: false}
-                    ])
-                    .setColor(Colors.Red)
-                    .setFooter({ text: `発見者: ${stop_discoverer.displayName} 発表時刻: ${now_time.displayName}` })
-                ]
-            });
-        } else if (interaction.options.getSubcommand() === 'train_restart') {
-            //遅延解除or運転再開
+                await interaction.client.channels.cache.get('1195747894704209960').send({
+                    content: "📣鉄道状態アナウンスが発令されました",
+                    embeds: [
+                        new EmbedBuilder()
+                            .setAuthor({ name: `通達者: ${interaction.member.displayName}` })
+                            .setTitle('鉄道状態アナウンス[運転停止/遅延発生情報 ID:')
+                            .setFields([
+                                { name: '今回のタイプ:', value: stop_type, inline: false },
+                                { name: '💥停止区間:', value: stop_section, inline: false },
+                                { name: '🧨原因:', value: stop_reason, inline: false },
+                                { name: '🔨予想修理時間:', value: stop_repire, inline: false },
+                                { name: '💬詳細情報:', value: stop_info, inline: false }
+                            ])
+                            .setColor(Colors.Red)
+                            .setFooter({ text: `発見者: ${stop_discoverer.displayName} 発表時刻: ${now_time.displayName}` })
+                    ]
+                });
+            } else if (interaction.options.getSubcommand() === 'train_restart') {
+                //遅延解除or運転再開
 
-            //変数に代入
-            var restart_type = interaction.options.getString("restart_type");
-            const restart_section = interaction.options.getString("restart_section");
-            const restart_repire = interaction.options.getString("restart_repire");
-            const restart_now = interaction.options.getString("restart_now") ?? "遅延再開のためなし。";
-            const restart_info = interaction.options.getString("restart_info") ?? "なし";
+                //変数に代入
+                var restart_type = interaction.options.getString("restart_type");
+                const restart_section = interaction.options.getString("restart_section");
+                const restart_repire = interaction.options.getString("restart_repire");
+                const restart_now = interaction.options.getString("restart_now") ?? "遅延再開のためなし。";
+                const restart_info = interaction.options.getString("restart_info") ?? "なし";
 
-            //変数変換
-            if (restart_type == "restart_trainstop") {
-                restart_type = "運転再開";
-            } else if (restart_type == "no_traindelay") {
-                restart_type = "遅延解除";
-            } else {
+                //変数変換
+                if (restart_type == "restart_trainstop") {
+                    restart_type = "運転再開";
+                } else if (restart_type == "no_traindelay") {
+                    restart_type = "遅延解除";
+                } else {
+                    await interaction.reply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle('Error :', stop_type)
+                                .setColor(Colors.DarkPurple)
+                        ]
+                    });
+                }
+
+                //送信
                 await interaction.reply({
                     embeds: [
                         new EmbedBuilder()
-                        .setTitle('Error :', stop_type)
-                        .setColor(Colors.DarkPurple)
+                            .setTitle('鉄道状態アナウンス[運転再開/遅延解除情報]を発表しました。')
+                            .setColor(Colors.Blue)
+                    ]
+                });
+
+                await interaction.client.channels.cache.get('1195747894704209960').send({
+                    content: "📣鉄道状態アナウンスが発令されました",
+                    embeds: [
+                        new EmbedBuilder()
+                            .setAuthor({ name: `通達者: ${interaction.member.displayName}` })
+                            .setTitle('鉄道状態アナウンス[運転再開/遅延解除情報情報]ID:')
+                            .setFields([
+                                { name: '今回のタイプ:', value: restart_type, inline: false },
+                                { name: '✅解除区間:', value: restart_section, inline: false },
+                                { name: '🛠️修理期間:', value: restart_repire, inline: false },
+                                { name: '🚃鉄道状態(遅延再開ならなし):', value: restart_now, inline: false },
+                                { name: '💬詳細情報:', value: restart_info, inline: false }
+
+                            ])
+                            .setColor(Colors.Aqua)
                     ]
                 });
             }
-
-            //送信
-            await interaction.reply({
-                embeds: [
-                    new EmbedBuilder()
-                    .setTitle('鉄道状態アナウンス[運転再開/遅延解除情報]を発表しました。')
-                    .setColor(Colors.Blue)
-                ]
-            });
-
-            await interaction.client.channels.cache.get('1195747894704209960').send({
-                content: "📣鉄道状態アナウンスが発令されました",
-                embeds: [
-                    new EmbedBuilder()
-                    .setAuthor({ name: `通達者: ${interaction.member.displayName}` })
-                    .setTitle('鉄道状態アナウンス[運転再開/遅延解除情報情報]ID:')
-                    .setFields([
-                        { name: '今回のタイプ:', value: restart_type ,inline: false},
-                        { name: '✅解除区間:', value: restart_section ,inline: false},
-                        { name: '🛠️修理期間:', value: restart_repire ,inline: false},
-                        { name: '🚃鉄道状態(遅延再開ならなし):', value: restart_now ,inline: false},
-                        { name: '💬詳細情報:', value: restart_info ,inline: false}
-                        
-                    ])
-                    .setColor(Colors.Aqua)
-                ]
-            });
-        }
     }
 };
--- commands/utility/report.js
+++ commands/utility/report.js
@@ -1,34 +1,34 @@
 const { SlashCommandBuilder, channelMention, userMention, EmbedBuilder, Colors, PermissionsBitField, ChatInputCommandInteraction } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-    //コマンド名前
-		.setName('trainreport')
+    data: new SlashCommandBuilder()
+        //コマンド名前
+        .setName('trainreport')
         //コマンドの説明
-		.setDescription('Reportコマンド。詳細はWoodry鉄道鯖、コマンドを参照')
-            .addSubcommand((subcommand) =>
+        .setDescription('Reportコマンド。詳細はWoodry鉄道鯖、コマンドを参照')
+        .addSubcommand((subcommand) =>
             subcommand
-            .setName("add")
-            .setDescription("新しくインシデント報告チャンネルを作成する")
-            
-            .addStringOption((option) => option
-            .setName('addreportchannel')
-            .setDescription('インシデント報告名')
-            .setRequired(true),
-            )
-            )
-            .addSubcommand((subcommand) =>
+                .setName("add")
+                .setDescription("新しくインシデント報告チャンネルを作成する")
+
+                .addStringOption((option) => option
+                    .setName('addreportchannel')
+                    .setDescription('インシデント報告名')
+                    .setRequired(true),
+                )
+        )
+        .addSubcommand((subcommand) =>
             subcommand
-            .setName("closed")
-            .setDescription("インシデント報告チャンネルを閉じる")
-            ),
+                .setName("closed")
+                .setDescription("インシデント報告チャンネルを閉じる")
+        ),
     /**
      * インタラクションが作成されたときに呼ばれるイベントのリスナー関数
      * @param {ChatInputCommandInteraction} interaction
      */
-	async execute(interaction) {
+    async execute(interaction) {
         if (interaction.options.getSubcommand() === 'add') {
-			
+
             const addreportchannel = interaction.options.getString("addreportchannel");    //IDには、「.setName」で指定した名前を指定
             const channel = await interaction.guild.channels.create({
                 name: addreportchannel,
@@ -43,58 +43,59 @@ module.exports = {
                         allow: [PermissionsBitField.Flags.ViewChannel]
                     },
                     {
-                      id: interaction.user.id,
-                      allow: [PermissionsBitField.Flags.ViewChannel]
+                        id: interaction.user.id,
+                        allow: [PermissionsBitField.Flags.ViewChannel]
                     }
-                  ]
+                ]
             });
-            await interaction.reply({ embeds: [
-                new EmbedBuilder()
-                .setTitle('インシデント報告用チャンネルを作成しました。')
-                .setDescription('カテゴリー:インシデントログで確認してください')
-                .addFields(
-                    { name: 'チャンネル名:', value: channelMention(channel.id)}
-                )
-                .setColor(Colors.Red)
-            ]
-        });
+            await interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle('インシデント報告用チャンネルを作成しました。')
+                        .setDescription('カテゴリー:インシデントログで確認してください')
+                        .addFields(
+                            { name: 'チャンネル名:', value: channelMention(channel.id) }
+                        )
+                        .setColor(Colors.Red)
+                ]
+            });
             await interaction.client.channels.cache.get('1195747894704209960').send({
                 content: userMention('1074320635855126538'),
                 embeds: [
                     new EmbedBuilder()
-                    .setTitle('インシデントが発生しました。')
-                    .setDescription('対応してください。')
-                    .addFields(
-                        { name: 'インシデント報告名:', value: channelMention(channel.id)}
-                    )
-                    .setColor(Colors.Blue)
-                ]
-            });
-		} else if (interaction.options.getSubcommand() === 'closed') {
-            if (interaction.channel.parentId !== '1195712382798930022') {
-                await interaction.reply(`インシデントログでのみ実行可能です。`);
-            }
-            if (!interaction.member.roles.cache.has('1195719181656662148') && !interaction.member.roles.cache.has('1196278442153488485') && !interaction.member.roles.cache.has('1192986048213553213')) {
-                await interaction.reply({ content: `貴方は実行権限を持ち合わせていません。\nご不明な点があれば\`/report crwdia\`でお問い合わせください。`, ephemeral: true });
-            }
-            else {
-                await interaction.reply(`このチャンネルを閉じます・・・`);
-                await interaction.client.channels.cache.get('1195747894704209960').send({
-                    content: userMention('1074320635855126538'),
-                    embeds: [
-                        new EmbedBuilder()
-                        .setTitle('解決済み。')
+                        .setTitle('インシデントが発生しました。')
+                        .setDescription('対応してください。')
                         .addFields(
-                            { name: 'インシデント報告名:', value: interaction.channel.name}
+                            { name: 'インシデント報告名:', value: channelMention(channel.id) }
                         )
                         .setColor(Colors.Blue)
-                    ]
-                });
+                ]
+            });
+        } else if (interaction.options.getSubcommand() === 'closed') {
+            if (interaction.channel.parentId !== '1195712382798930022') {
+                await interaction.reply(`インシデントログでのみ実行可能です。`);
+            } else
+                if (!interaction.member.roles.cache.has('1195719181656662148') && !interaction.member.roles.cache.has('1196278442153488485') && !interaction.member.roles.cache.has('1192986048213553213')) {
+                    await interaction.reply({ content: `貴方は実行権限を持ち合わせていません。\nご不明な点があれば\`/report crwdia\`でお問い合わせください。`, ephemeral: true });
+                }
+                else {
+                    await interaction.reply(`このチャンネルを閉じます・・・`);
+                    await interaction.client.channels.cache.get('1195747894704209960').send({
+                        content: userMention('1074320635855126538'),
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle('解決済み。')
+                                .addFields(
+                                    { name: 'インシデント報告名:', value: interaction.channel.name }
+                                )
+                                .setColor(Colors.Blue)
+                        ]
+                    });
 
-                setTimeout(async () => {
-                    await interaction.channel.delete();
-                }, 3e3);
-            }
-		}
-	},
+                    setTimeout(async () => {
+                        await interaction.channel.delete();
+                    }, 3e3);
+                }
+        }
+    },
 };
