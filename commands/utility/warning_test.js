48e958703f429f29d417a95917c89ffceb1909c6
2 1 <InotitoKokoro@gmail.com>
2024-05-26 22:54:45 +0900

    一時的にアップロード
    それだけ。

--- commands/utility/warning_test.js
+++ commands/utility/warning_test.js
@@ -1,71 +1,102 @@
-const { joinVoiceChannel, entersState, VoiceConnectionStatus, createAudioResource, StreamType, createAudioPlayer, AudioPlayerStatus, NoSubscriberBehavior, generateDependencyReport } = require("@discordjs/voice");
+const { joinVoiceChannel, entersState, VoiceConnectionStatus, createAudioResource, StreamType,
+    createAudioPlayer, AudioPlayerStatus, NoSubscriberBehavior, generateDependencyReport, AudioPlayer,
+    AudioResource } = require("@discordjs/voice");
+const discord_id = require("../../id")
+console.log(generateDependencyReport());
 
 const { SlashCommandBuilder, EmbedBuilder, Colors, ChatInputCommandInteraction } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
-    //コマンド名前
+        //コマンド名前
         .setName('warning')
         //コマンドの説明
         .setDescription('Replies with Pong!')
         .addSubcommand((subcommand) =>
-        subcommand
-            .setName("test")
-            .setDescription("おはよう")
+            subcommand
+                .setName("test")
+                .setDescription("おはよう")
         ),
 
     async execute(interaction) {
-    
-    const guild = interaction.guild;
-    const member = await guild.members.fetch(interaction.member.id);
-    const memberVC = member.voice.channel;
-    const status = ["●Loading Sounds...", `●Connecting to ${memberVC}...`];
-    const p = interaction.reply(status.join("\n"));
+
+        const guild = interaction.guild;
+        const member = await guild.members.fetch(interaction.member.id);
+        const memberVC = member.voice.channel;
 
 
-  const connection = joinVoiceChannel({
-    guildId: guild.id,
-    channelId: memberVC.id,
-    adapterCreator: guild.voiceAdapterCreator,
-    selfMute: false,
-  });
+        const connection = joinVoiceChannel({
+            guildId: guild.id,
+            channelId: memberVC.id,
+            adapterCreator: guild.voiceAdapterCreator,
+            selfMute: false,
+        });
+        await interaction.reply("Play WARNING_1...");//返信
 
-
-  const resource = createAudioResource("https://cdn.discordapp.com/attachments/1244260019227070514/1244260442365493319/WARNING_1.mp3?ex=66547756&is=665325d6&hm=75d12fa901d1a9ed0dce6c42aa1c6158f1e223c520570d07fa549c8beda721d3&",{
+        await getAttachmentURL(interaction, discord_id.warn_sounds, discord_id.warn_message["WARNING_1"]).then(async (url) => {
+            /*** @type {AudioResource}*/
+            let resource = createAudioResource(url, {
                 inputType: StreamType.Arbitrary,
             });
-
-
-  const player = createAudioPlayer({
-    behaviors: {
-      noSubscriber: NoSubscriberBehavior.Pause,
-    },
-  });
-
-
-
-  player.play(resource);
-  const promises = [];
-  promises.push(entersState(player, AudioPlayerStatus.AutoPaused, 1000 * 10).then(() => status[0] += "Done!"));
-  promises.push(entersState(connection, VoiceConnectionStatus.Ready, 1000 * 10).then(() => status[1] += "Done!"));
-  await Promise.race(promises);
-  await p;
-  await Promise.all([...promises, interaction.editReply(status.join("\n"))]);
-  connection.subscribe(player);
-  await entersState(player, AudioPlayerStatus.Playing, 100);
-
-  await interaction.editReply("Playing");
-  await entersState(player, AudioPlayerStatus.Idle, 2 ** 31 - 1);
-  await interaction.editReply("End");
-  let count = 0;
-            player.on(AudioPlayerStatus.Idle, () => {
-                count += 1;
-                console.log(`再生回数: ${count}`);
-
-                if (count < 5) {
-                    player.play(resource); // リソースを再再生
-                } else {
-                    connection.destroy(); // 接続を切断
-                }
+            /*** @type {AudioPlayer}*/
+            let player = createAudioPlayer({
+                behaviors: {
+                    noSubscriber: NoSubscriberBehavior.Pause,
+                },
             });
-  }
+
+
+            let play = async (startup) => {
+                /**resource = createAudioResource(url, {
+                    inputType: StreamType.Arbitrary,
+                });
+                player = createAudioPlayer({
+                    behaviors: {
+                        noSubscriber: NoSubscriberBehavior.Pause,
+                    },
+                });**/
+                let _resource = createAudioResource(url, {
+                    inputType: StreamType.Arbitrary,
+                });
+                player.play(_resource);
+                /TODO: #2 音声の繰り返し再生時の挙動を修正する/
+                if (startup) {
+                    const promises = [];
+                    promises.push(entersState(player, AudioPlayerStatus.AutoPaused, 1000 * 10));//10秒 or 自動停止されるまで処理停止
+                    promises.push(entersState(connection, VoiceConnectionStatus.Ready, 1000 * 10));//10秒 or 準備完了まで処理停止
+                    await Promise.race(promises);//上2つのどちら速い方が終わるまで待つ、ただし片方完了したらもう片方は実行停止
+                    await Promise.all([...promises]);//上2つが終わるまで待つ
+                    connection.subscribe(player);//スピーカーを刺す
+                }
+                await entersState(player, AudioPlayerStatus.Playing, 100);
+                await entersState(player, AudioPlayerStatus.Idle, 2 ** 31 - 1);//2,147,483,647ミリ秒後にタイムアウト
+                //connection.disconnect();
+            }
+            for (let count = 0; count < 5; count++) {
+                await play(count == 0);
+                console.log(`replay(再生回数: ${count})`)
+                //player.stop(true)
+                ////刺した状態でさらに刺すことは不可なので抜く。
+                if (count == 4) {
+                    connection.destroy();// 接続を切断
+                }
+            }
+        })
+    }
+}
+/**
+ * @param {ChatInputCommandInteraction} interaction 
+ * @param {string} channelID 
+ * @param {string} messageID 
+ * @returns {Promise<string>}
+ */
+let getAttachmentURL = (interaction, channelID, messageID) => {
+    return new Promise((resolve, reject) => {
+        interaction.guild.channels.fetch(discord_id.warn_sounds).then(channel => {
+            channel.messages.fetch(discord_id.warn_message.WARNING_1).then(message => {
+                message.attachments.map((value, key) => {
+                    resolve(value.url)
+                })
+            }).catch((re) => { reject(re) })
+        }).catch((re) => { reject(re) })
+    })
 }
